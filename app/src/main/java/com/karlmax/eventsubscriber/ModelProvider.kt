ee7f2d975a54f977ad3088cab2755b4f6da38392
Sascha Zieger <zieger@karlmax-berlin.com>
2018-03-20 16:01:04 +0100

    fixed start and end date

--- app/src/main/java/com/karlmax/eventsubscriber/ModelProvider.kt
+++ app/src/main/java/com/karlmax/eventsubscriber/ModelProvider.kt
@@ -2,20 +2,17 @@ package com.karlmax.eventsubscriber
 
 import com.karlmax.eventsubscriber.api.FacebookApiImplementation
 import com.karlmax.eventsubscriber.entities.Event
-import com.karlmax.eventsubscriber.entities.EventPlace
 
 object ModelProvider {
 
-    fun provideEvents(organizersName : String, listener: (ArrayList<Event>) -> Unit) {
-        FacebookApiImplementation.getEventsFromOrganizer(organizersName, listener)
-    }
-
     private val cachedEvents = ArrayList<Event>()
 
-    init {
-        val place = EventPlace(0, "Krasser Schuppen", "Deutschland", "Berlin", "Anderstraße 5", "1337", 0.0, 0.0)
-        cachedEvents.add(Event(1, "just a test", "", 0, 0, place))
-        cachedEvents.add(Event(2, "just another test", "", 0, 0, place))
+    fun provideEvents(organizersName : String, listener: (ArrayList<Event>) -> Unit) {
+        FacebookApiImplementation.getEventsFromOrganizer(organizersName) {
+            cachedEvents.clear()
+            cachedEvents.addAll(it)
+            listener(it)
+        }
     }
 
     fun provideEventForId(eventId: Long, listener: (Event?) -> Unit) = listener(cachedEvents.find { it.id == eventId })
--- app/src/main/java/com/karlmax/eventsubscriber/entities/Event.kt
+++ app/src/main/java/com/karlmax/eventsubscriber/entities/Event.kt
@@ -2,21 +2,22 @@ package com.karlmax.eventsubscriber.entities
 
 import android.content.Context
 import android.text.format.DateFormat
+import com.google.gson.annotations.SerializedName
 import com.karlmax.eventsubscriber.R
 import java.util.*
 
 data class Event(val id: Long,
                  val name: String,
                  val description: String,
-                 val startTime: Long,
-                 val endTime: Long,
+                 @SerializedName("start_time") val startTime: String,
+                 @SerializedName("end_time") val endTime: String,
                  val place: EventPlace) {
 
     fun getStartTimeFormatted(context: Context) =
-            context.getString(R.string.event_start_date, getDate(startTime))!!
+            context.getString(R.string.event_start_date, startTime)!!
 
     fun getEndTimeFormatted(context: Context) =
-            context.getString(R.string.event_end_date, getDate(endTime))!!
+            context.getString(R.string.event_end_date, endTime)!!
 
     private fun getDate(time: Long): String {
         val cal = Calendar.getInstance(Locale.ENGLISH)
